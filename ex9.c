c2fcc06da30d341ad5fe9dc7e1c1e2abab7a80e2
ClaraFranchello <claraguifran@gmail.com>
2026-04-26 07:55:16 -0300

    Add files via upload

---
+++ ex9.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main(){
+
+    int altura, base, espaco, aux, div, fim, val;
+    
+    printf("Digite a altura: ");
+    scanf("%i", &altura);
+    
+    aux = 1;
+    fim = 0;
+    div = altura / 2;
+    val = div;
+    if(altura >= 3){
+        
+        for(int i = 1 ; i <= altura; i++){
+            if(fim == 0){
+                for(int espaco = 1; espaco <= div; espaco++){
+                    if(espaco <= div && fim == 0){
+                        printf(" ");
+                        if(espaco == div && aux < altura){
+                            for(int emoj = 1; emoj <=aux; emoj++){
+                                printf("X");
+                            }   
+                        printf("\n");
+                        espaco = 0;
+                        div = div - 1;
+                        aux = aux + 2;
+                        }
+                    }
+                }
+            }
+                if(div == 0 && fim == 0){
+                        fim = 1;
+                        div = 1;
+                        for(int base = 1; base <= altura; base++){
+                            printf("X");
+                        }
+                        printf("\n");
+                }
+                if(fim == 1){
+                    for(int espaco1 = 1; espaco1 <= div; espaco1++ ){
+                        if(espaco1 <= div && div <= val){
+                            printf(" ");
+                            if(espaco1 == div && aux >= 1){
+                                aux = aux - 2;
+                                for(int emoj = 1; emoj <=aux; emoj++){
+                                    printf("X");
+                                }   
+                                printf("\n");
+                                espaco1 = 0;
+                                div = div + 1;
+                            }
+                        }
+                    }
+                }
+                
+            
+           
+        
+
+        }
+    }
+
+    return 0;
+}
